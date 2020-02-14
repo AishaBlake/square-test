957ab62eb3425fc73693c25ce88ecf7fbcab03c0
Aisha Blake <aisha.g.blake@gmail.com>
2020-02-14 13:45:01 -0500

    Delete unused files

--- gatsby-ssr.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export const onRenderBody = ({ setHeadComponents }) => {
-  setHeadComponents([
-    <script src='https://js.squareup.com/v2/paymentform'></script>
-  ]);
-};
